54ed49508279cef5a80b196f987ceef008350c62
Chris <chris.overgaauw@gmail.com>
2016-11-27 00:39:16 +0100

    initial app - slideshow

---
+++ app.js
@@ -0,0 +1,30 @@
+new Vue({
+    el: 'image-slider',
+    data: {
+        images: ['assets/images/androidPhone.png','http://i.imgur.com/vYdoAKu.jpg', 'http://i.imgur.com/PUD9HQL.jpg', 'http://i.imgur.com/Lfv18Sb.jpg', 'http://i.imgur.com/tmVJtna.jpg', 'http://i.imgur.com/ZfFAkWZ.jpg'],
+        currentNumber: 0,
+        timer: null
+    },
+
+    ready: function () {
+//        this.startRotation();
+    },
+
+    methods: {
+//        startRotation: function() {
+//            this.timer = setInterval(this.next, 3000);
+//        },
+
+//        stopRotation: function() {
+//            clearTimeout(this.timer);
+//            this.timer = null;
+//        },
+
+        next: function() {
+            this.currentNumber += 1
+        },
+        prev: function() {
+            this.currentNumber -= 1
+        }
+    }
+});
